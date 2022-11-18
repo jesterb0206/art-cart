176b195b421f04455d55e92a91f900e4525baa6d
jesterb0206 <jesterb@seattleu.edu>
2022-11-18 01:40:02 -0800

    Slight tweak to auth.js and added a potentially useful helper function

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,8 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
 
-  if (!req.session.logged_in) {
+  if (!req.session.user_id) {
+    console.log('You are not logged in.');
     res.redirect('/login');
   } else {
     next();
