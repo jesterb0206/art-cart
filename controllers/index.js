504d07e3e2d9a120588c2a8dec664f08ff94b8e5
Timothy Barnaby <tbarnaby1@gmail.com>
2022-11-17 23:29:21 -0600

    debugged inital routing for users

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
-const userRoutes = require('./api/userRoutes');
+// const userRoutes = require('./api/user-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/api', userRoutes);
+// router.use('/api', userRoutes);
 
 module.exports = router;
