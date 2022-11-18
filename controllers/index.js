fa6783be1db1e091859f710ab30a6108edd8a2b9
jesterb0206 <jesterb@seattleu.edu>
2022-11-18 01:04:03 -0800

    Added signup page view route and modified controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-// const userRoutes = require('./api/user-routes');
+const homeRoutes = require('./home-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-// router.use('/api', userRoutes);
 
 module.exports = router;
