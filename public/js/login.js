e3ad27d5813d37e93b056ee197bf2ab6a1c3be6f
jesterb0206 <jesterb@seattleu.edu>
2022-11-18 01:12:03 -0800

    Another way we could go about creating public/js/login.js (made sure to comment out my idea)

--- public/js/login.js
+++ public/js/login.js
@@ -54,3 +54,32 @@ document
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
+
+// Another idea on how we could create login.js
+
+/* const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    console.log(response);
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      $("#invalid").removeClass("hide");
+    }
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler); */
