08c826ddcd391db2468db3bd4676e91eb369036e
Alex Lee <98978477+ayesslee@users.noreply.github.com>
2022-11-21 21:18:13 -0800

    update post route

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,17 +12,17 @@ router.get('/', async (req, res) => {
   }
 });
 
-/*router.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const commentData = await Comment.create({
-      comment: req.body.id,
+      comment: req.body.comment,
       user_id: req.body.user_id,
       post_id: req.body.post_id,
     });
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+      // req.session.user_id = userData.id;
+      // req.session.logged_in = true;
 
       res
         .status(200)
@@ -32,6 +32,6 @@ router.get('/', async (req, res) => {
     console.log(err);
     res.status(500).json(err);
   }
-});*/
+});
 
 module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -12,4 +12,26 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+      const postData = await Post.create({
+        post_title: req.body.post_title,
+        post_body: req.body.post_body,
+        user_id: req.body.user_id,
+      });
+  
+      req.session.save(() => {
+        // req.session.user_id = userData.id;
+        // req.session.logged_in = true;
+  
+        res
+          .status(200)
+          .json({ user: postData, message: 'Postmates' });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
