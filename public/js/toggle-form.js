eb5418c320202bbe53562dc68cf8a447cea5b094
jesterb0206 <jesterb@seattleu.edu>
2022-11-24 00:49:01 -0800

    Got Toggle Menu & Login/Register Form To Work In Handlebars

---
+++ public/js/toggle-form.js
@@ -0,0 +1,14 @@
+var LoginForm = document.getElementById('LoginForm');
+var RegForm = document.getElementById('RegForm');
+var Indicator = document.getElementById('indicator');
+
+function register() {
+  RegForm.style.transform = 'translateX(0px)';
+  LoginForm.style.transform = 'translateX(0px)';
+  Indicator.style.transform = 'translateX(100px)';
+}
+function login() {
+  RegForm.style.transform = 'translateX(300px)';
+  LoginForm.style.transform = 'translateX(300px)';
+  Indicator.style.transform = 'translateX(0px)';
+}
