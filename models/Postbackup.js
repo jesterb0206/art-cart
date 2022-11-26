bfb4476800220a33ccb0514b11b33c461b3990e5
fiamett <f.ignite@hotmail.com>
2022-11-25 19:39:40 -0800

    chaned post model and made backup of old

--- models/Postbackup.js
+++ models/Postbackup.js
@@ -15,31 +15,12 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    post_img: {
-      type: DataTypes.STRING,
+    post_body: {
+      // VARCHAR(1234), default is VARCHAR(255) I believe
+      type: DataTypes.STRING(1234),
       allowNull: false,
     },
-    post_price: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    post_medium: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    post_size: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    post_year: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    post_signed: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-    // Added date_created to our post model
+    // Added date_created to our Post model
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
@@ -67,7 +48,7 @@ Post.init(
     freezeTableName: true,
     // I believe 'underscored: true' makes everything in snake case
     underscored: true,
-    modelName: 'Post',
+    modelName: 'post',
   }
 );
 
--- seeds/postData.js
+++ seeds/postData.js
@@ -2,14 +2,24 @@ const { Post } = require('../models');
 
 const postData = [
   {
-    post_title: 'this is a test post',
-    post_body: 'bodyodyody',
+    post_title: 'By the Coast - Seascape Oil Painting by Darko Topalski',
+    post_img: 'test img 2',
+    post_price: 217.0,
+    post_medium: 'oil on canvas',
+    post_size: '20" x 28',
+    post_year: 2018,
+    post_signed: true,
     user_id: 1,
   },
   {
-    post_title: 'this is a 2nd test post',
-    post_body: 'body?',
-    user_id: 2,
+    post_title: 'test title',
+    post_img: 'test img 2',
+    post_price: 523.23,
+    post_medium: 'thoughts and prayers',
+    post_size: 'as big as you want',
+    post_year: 1800,
+    post_signed: true,
+    user_id: 1,
   },
 ];
 
